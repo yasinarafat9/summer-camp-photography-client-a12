0d7960163aa473258c36647e9a608c12d82f83e3
yasinarafat9 <yasinarafat9612@gmail.com>
2023-06-07 23:19:25 +0600

    react router applied pages and shared folder created

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <button className="btn">Button</button>
-        <button className="btn btn-neutral">Neutral</button>
-        <button className="btn btn-primary">Button</button>
-        <button className="btn btn-secondary">Button</button>
-        <button className="btn btn-accent">Button</button>
-        <button className="btn btn-ghost">Button</button>
-        <button className="btn btn-link">Button</button>
+       
       </div>
     </>
   )
