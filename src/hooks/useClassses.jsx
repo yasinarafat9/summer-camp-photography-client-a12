5fedbef15b9dae2d9e91234f97bf6473b632eea1
yasinarafat9 <yasinarafat9612@gmail.com>
2023-06-08 20:58:54 +0600

    classes json file created

---
+++ src/hooks/useClassses.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const useClasses = () => {
+    const [classes, setClasses] = useState([])
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch('classes.json')
+        .then(res => res.json())
+        .then(data => {
+            setClasses(data);
+            setLoading(false);
+        })
+    },[])
+    return [classes, loading]
+
+
+}
+
+export default useClasses;
