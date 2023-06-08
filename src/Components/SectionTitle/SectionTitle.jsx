4535a0d69f71944c704ec5d827e446e082b7c219
yasinarafat9 <yasinarafat9612@gmail.com>
2023-06-08 12:31:29 +0600

    .eslintrc react prop validation disabled

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+
+
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div>
+            <h2>{heading}</h2>
+            <h4>{subHeadingb</h4>
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import img1 from '../../../assets/Images/Banner/img (1).webp'
-import img2 from '../../../assets/Images/Banner/img (2).jpg'
-import img3 from '../../../assets/Images/Banner/img (3).webp'
-import img4 from '../../../assets/Images/Banner/img (4).webp'
-import img5 from '../../../assets/Images/Banner/img (5).jpg'
+import img1 from '../../../assets/Images/Home/Banner/img (1).webp'
+import img2 from '../../../assets/Images/Home/Banner/img (2).jpg'
+import img3 from '../../../assets/Images/Home/Banner/img (3).webp'
+import img4 from '../../../assets/Images/Home/Banner/img (4).webp'
+import img5 from '../../../assets/Images/Home/Banner/img (5).jpg'
 
 const Banner = () => {
     return (
