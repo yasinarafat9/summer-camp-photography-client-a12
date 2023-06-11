65881c1e261650bfaddcc7ad74511999e1fb7a5a
yasinarafat9 <yasinarafat9612@gmail.com>
2023-06-11 23:35:29 +0600

    added dashboard home links

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 
 const Dashboard = () => {
     return (
         <div>
-            <div className="drawer lg:drawer-open">
+            <div className="drawer lg:drawer-open ">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     {/* Page content here */}
@@ -14,10 +14,16 @@ const Dashboard = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                    <ul className="menu p-4 w-80 h-full bg-black opacity-50 text-white">
                         {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                        <NavLink className=' font-semibold text-lg' to='/dashboard/myclass'>My Selected Classes</NavLink>
+                        <NavLink className=' font-semibold text-lg' to='/dashboard/myclass'>My Enrolled Classes</NavLink>
+                        <br />
+                        <hr />
+                        <br />
+                        <NavLink className="font-semibold text-lg" to=''>Home</NavLink>
+                        <NavLink className="font-semibold text-lg" to='/instructors'>Instructors</NavLink>
+                        <NavLink className="font-semibold text-lg" to='/allClasses'>Classes</NavLink>
                     </ul>
 
                 </div>
